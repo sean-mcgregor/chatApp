6974685b539a7616ed944831d8eb40e5126e6acf
sean-mcgregor <worksmcg@gmail.com>
2021-04-22 17:34:36 +0300

    Part 3 working

--- androidChat/app/src/main/java/com/sean/chat/MainActivity.kt
+++ androidChat/app/src/main/java/com/sean/chat/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
+import android.text.InputType
 import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.getSystemService
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -20,8 +27,11 @@ import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG: String = MainActivity::class.java.name
     private lateinit var messages: ArrayList<String>
     private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private var currentUser: FirebaseUser? = null
     private lateinit var edMessage: EditText
     private lateinit var rcMessageList: RecyclerView
 
@@ -33,7 +43,8 @@ class MainActivity : AppCompatActivity() {
         edMessage = findViewById(R.id.messageText)
         rcMessageList = findViewById(R.id.messageList)
         database = Firebase.database.reference
-        messages = arrayListOf()
+        auth = Firebase.auth
+        messages = arrayListOf<String>()
 
         edMessage.setOnKeyListener {v, keyCode, event ->
             if(keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
@@ -66,6 +77,60 @@ class MainActivity : AppCompatActivity() {
         rcMessageList.layoutManager = LinearLayoutManager(this)
         rcMessageList.adapter = MyAdapter(messages)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        loginDialog()
+    }
+
+    fun loginDialog(){
+        val builder = AlertDialog.Builder(this)
+
+        with(builder) {
+            setTitle("Log in")
+            val linearLayout: LinearLayout = LinearLayout(this@MainActivity)
+            linearLayout.orientation = LinearLayout.VERTICAL
+
+            val inputEmail: EditText = EditText(this@MainActivity)
+            inputEmail.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            inputEmail.hint = "Enter email"
+            linearLayout.addView(inputEmail)
+
+            val inputPw: EditText = EditText(this@MainActivity)
+            inputPw.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            inputPw.hint = "Enter password"
+            linearLayout.addView(inputPw)
+            builder.setView(linearLayout)
+
+            builder.setPositiveButton("OK"){dialog, which ->
+                login(inputEmail.text.toString(), inputPw.text.toString())
+            } .show()
+
+
+        }
+
+
+    }
+
+    fun login(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){task->
+                    if (task.isSuccessful){
+                        Log.d(TAG,"signInWithEmail:success")
+                        Toast.makeText(baseContext, "Authentification successful",
+                                Toast.LENGTH_SHORT).show()
+                        currentUser= auth.currentUser
+                    }else{
+                        Log.w(TAG, "signInWithEmail:failure" , task.exception)
+                        Toast.makeText(baseContext, "Authentification failed.",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
 
     fun addMessage(){
 
